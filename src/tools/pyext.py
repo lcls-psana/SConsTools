a6b1a01b0a3301454d12e445cd2f3e7004ae0d5b
David Schneider <davidsch@slac.stanford.edu>
2017-04-26 14:31:40 -0700

    set rpath when linking python extensions, search test release arch/python as well as arch/lib, likewise in conda environment, search python2.7/site-packages as well as lib. Fixes #1

--- src/tools/pyext.py
+++ src/tools/pyext.py
@@ -108,7 +108,8 @@ def pyext_coms(platform):
                        "$PYEXTCXXFLAGS $PYEXTCCFLAGS $_CCCOMCOM "\
                        "$_PYEXTCPPINCFLAGS $SOURCES"
         pyext_linkcom = "$PYEXTLINK -o $TARGET $PYEXTLINKFLAGS "\
-                        "$SOURCES $_LIBDIRFLAGS $_LIBFLAGS $_PYEXTRUNTIME"
+                        " -Wl,-rpath='$$ORIGIN/../lib:$$ORIGIN/../python:$CONDA_ENV_PATH/lib:$CONDA_ENV_PATH/lib/python2.7/site-packages' "\
+                        "$SOURCES $_LIBDIRFLAGS $_LIBFLAGS $_PYEXTRUNTIME" 
 
     if platform == 'darwin':
         pyext_linkcom += ' $_FRAMEWORKPATH $_FRAMEWORKS $FRAMEWORKSFLAGS'
